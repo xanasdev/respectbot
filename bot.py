3b06f10e6158e8a37b360847d2d47b65e2083b69
Magomed <mshuribitov@mail.ru>
2024-11-27 19:13:01 +0300

    bot files change

--- bot.py
+++ bot.py
@@ -1,1 +1,28 @@
-print("hello world")
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7586671721:AAHIo6viRAlnrq3U259d-sas_A3oJu90SwI")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+@dp.message(Command("gasan"))
+async def cmd_start(message: types.Message):
+    await message.answer("Жопу закрой")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
